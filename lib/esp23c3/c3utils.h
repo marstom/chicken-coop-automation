8edfe1b50856271e5ad44f521cf9b4bfd3e703bd
Tomasz Marszalek <marstomek@gmail.com>
2025-09-25 19:58:21 +0200

    clean the code

--- lib/esp23c3/c3utils.h
+++ lib/esp23c3/c3utils.h
@@ -3,19 +3,19 @@ C3 utils by Tom
 */
 
 
-namespace my {
+// namespace my {
 
-    // PIN definitions
-    constexpr int D0 = 2;
-    constexpr int D1 = 3;
-    constexpr int D2 = 28;
-    constexpr int D3 = 29;
-    constexpr int D4 = 4;
-    constexpr int D5 = 5;
-    constexpr int D6 = 11;
-    constexpr int D7 = 12;
-    constexpr int D8 = 13;
-    constexpr int D9 = 14;
-    constexpr int D10 = 15;
+//     // PIN definitions
+//     constexpr int D0 = 2;
+//     constexpr int D1 = 3;
+//     constexpr int D2 = 28;
+//     constexpr int D3 = 29;
+//     constexpr int D4 = 4;
+//     constexpr int D5 = 5;
+//     constexpr int D6 = 11;
+//     constexpr int D7 = 12;
+//     constexpr int D8 = 13;
+//     constexpr int D9 = 14;
+//     constexpr int D10 = 15;
 
-}
+// }
--- src/main_c3_relay_controller.cpp
+++ src/main_c3_relay_controller.cpp
@@ -26,18 +26,18 @@
 #define RELAY_1_SET_TOPIC "relay/1/set"
 
 // BME temperature and humidity sensor, connected to i2c bus in current setup
-#define BME_SCK my::D8
-#define BME_MISO my::D9
-#define BME_MOSI my::D10
-#define BME_CS my::D7
+#define BME_SCK D8
+#define BME_MISO D9
+#define BME_MOSI D10
+#define BME_CS D7
 
 #define BME_TEMPERATURE_TOPIC "bme280/temperature"
 #define BME_PRESSURE_TOPIC "bme280/pressure"
 #define BME_HUMIDITY_TOPIC "bme280/humidity"
 #define BME_ALTITUDE_TOPIC "bme280/altitude"
 
-#define I2C_SDA my::D4
-#define I2C_SCL my::D5
+#define I2C_SDA D4
+#define I2C_SCL D5
 
 #define SEALEVELPRESSURE_HPA (1013.25)
 Adafruit_BME280 bme; // I2C
@@ -57,7 +57,7 @@ void setup()
 {
     Serial.begin(9600);
     my::connect_to_wifi_with_wait();
-    pinMode(my::D0, OUTPUT); // D0 as output
+    pinMode(D0, OUTPUT); // D0 as output
 
     client.setServer(host, 1883); // rpi server
     client.setCallback(mycallback);
@@ -108,7 +108,7 @@ void taskMQTT(void *pvParameters)
     for (;;)
     {
         client.loop(); // <--- processes incoming MQTT messages
-        vTaskDelay(pdMS_TO_TICKS(100));
+        vTaskDelay(pdMS_TO_TICKS(10));
     }
 }
 
@@ -132,11 +132,11 @@ void mycallback(char *topic, byte *message, unsigned int length)
         // in case of relay, the high state is OFF
         if (messageTemp == "ON")
         {
-            digitalWrite(my::D0, HIGH);
+            digitalWrite(D0, HIGH);
         }
         else
         {
-            digitalWrite(my::D0, LOW);
+            digitalWrite(D0, LOW);
         }
     }
 }
@@ -145,6 +145,8 @@ void taskReadBME280(void *pvParameters)
 {
     bool status;
     String payload;
+    char buf[16];
+
     //   Wire.begin(4, 5);
     //   while (!status) {
     //     status = bme.begin(0x77, &Wire);   // I2C or 0x77
@@ -153,42 +155,33 @@ void taskReadBME280(void *pvParameters)
     //   }
 
     
-    Wire.begin(4, 5);
+    Wire.begin(D4, D5); // 6 7
+    
     if (!bme.begin(0x76, &Wire))
     {
         if (!bme.begin(0x77, &Wire))
         {
             Serial.println("Could not find a valid BME280 sensor, check wiring!");
-            while (1)
-                delay(10);
+            // while (1)
+                // delay(10);
+            vTaskDelete(NULL); // delete task immediately if fails
         }
     }
     for (;;)
     {
-        // Serial.print("Temperature = ");
-        // Serial.print(bme.readTemperature());
-        String t = String(bme.readTemperature());
-        Serial.print(t);
-        client.publish(BME_TEMPERATURE_TOPIC, t.c_str(), true);
-        Serial.println(" *C");
+        // String t = String(bme.readTemperature());
+        snprintf(buf, sizeof(buf), "%.2f", bme.readTemperature());
+        client.publish(BME_TEMPERATURE_TOPIC, buf, true);
 
-        // Serial.print("Pressure = ");
-        // Serial.print(bme.readPressure() / 100.0F);
-        String p = String(bme.readPressure());
-        client.publish(BME_PRESSURE_TOPIC, p.c_str());
-        // Serial.println(" hPa");
 
-        // Serial.print("Approx. Altitude = ");
-        // Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
-        String a = String(bme.readAltitude(SEALEVELPRESSURE_HPA));
-        client.publish(BME_ALTITUDE_TOPIC, a.c_str());
-        // Serial.println(" m");
+        snprintf(buf, sizeof(buf), "%.2f", bme.readPressure());
+        client.publish(BME_PRESSURE_TOPIC, buf);
 
-        // Serial.print("Humidity = ");
-        // Serial.print(bme.readHumidity());
-        String h = String(bme.readHumidity());
-        client.publish(BME_HUMIDITY_TOPIC, h.c_str()); // add ,true to retain
-        // Serial.println(" %");
+        snprintf(buf, sizeof(buf), "%.2f", bme.readAltitude(SEALEVELPRESSURE_HPA));
+        client.publish(BME_ALTITUDE_TOPIC, buf);
+
+        snprintf(buf, sizeof(buf), "%.2f", bme.readHumidity());
+        client.publish(BME_HUMIDITY_TOPIC, buf); // add ,true to retain
 
         vTaskDelay(pdMS_TO_TICKS(1000));
     }
