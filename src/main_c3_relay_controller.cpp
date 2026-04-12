8e70d1f7d8a56f16e88b94e4e91a725020aa3fb3
Tomasz Marszalek <marstomek@gmail.com>
2026-04-12 15:02:54 +0200

    Start refactor relay controller

--- src/main_c3_relay_controller.cpp
+++ src/main_c3_relay_controller.cpp
@@ -13,10 +13,6 @@ This is door lock in my basement. BLE controlled plus WiFi controlled
 #include <HTTPClient.h>
 #include <PubSubClient.h>
 
-// for BME
-#include <Adafruit_Sensor.h>
-#include <Adafruit_BME280.h>
-
 // heap monitoring
 #include "esp_heap_caps.h"
 
@@ -34,50 +30,28 @@ This is door lock in my basement. BLE controlled plus WiFi controlled
 #include "secrets.h"
 
 // Hardware feature toggle, comment out hardware which you don't need
-// #define DEVICE_BME_280_ENABLED // enable temp & humidity & altitude & pressure sensor
 #define DEVICE_RELAY_ENABLED // enable relay controll
 #define BLE_ENABLED          // enable ble communication
 // #define ENABLE_MONITORING
-
-// Addressable RGB LED, driven by GPIO48.
-#define LED_PIN 48
 #define WDT_TIMEOUT 30
 
 #define RELAY_PIN D0
 
-// BME temperature and humidity sensor, connected to i2c bus in current setup
-#define BME_SCK D8
-#define BME_MISO D9
-#define BME_MOSI D10
-#define BME_CS D7
-
 // MQTT stuff
 #define THINGNAME "esp32-c3-basement-fhs232y3a43"
 #define PREFIX "basement/"
-#define BME_TEMPERATURE_TOPIC PREFIX "bme280/temperature"
-#define BME_TEMPERATURE_TOPIC PREFIX "bme280/temperature"
-#define BME_PRESSURE_TOPIC PREFIX "bme280/pressure"
-#define BME_HUMIDITY_TOPIC PREFIX "bme280/humidity"
-#define BME_ALTITUDE_TOPIC PREFIX "bme280/altitude"
 #define MQTT_LOG_TOPIC PREFIX "log/mydebug"
 #define STATUS_TOPIC PREFIX "status/read"
 #define RELAY_1_SET_TOPIC PREFIX "relay/1/set"
 
-#define I2C_SDA D4
-#define I2C_SCL D5
-
-#define SEALEVELPRESSURE_HPA (1013.25)
-Adafruit_BME280 bme; // I2C
+// #define I2C_SDA D4
+// #define I2C_SCL D5
 
 // MQTT broker settings
 const char *host = MQTT_HOST;
 const uint16_t mqttPort = MQTT_PORT;
 WiFiClient net;
 PubSubClient client(net);
-
-// telnet for print messages via wifi minitor
-// WiFiServer telnetServer(23);
-// WiFiClient telnetClient;
 
 // ------------- BLE support -------------
 // UUIDs
@@ -105,7 +79,7 @@ TaskHandle_t hRelayTask = NULL;
 /// make mqtt thread safe
 void mycallback(char *topic, byte *message, unsigned int length);
 void taskMQTT(void *pvParameters); // Spin all the time and keep receiving the messages!
-void taskReadBME280(void *pvParameters);
+// void taskReadBME280(void *pvParameters);
 void taskStackMonitor(void *pvParameters); // debug stack monitor for memory usage
 void taskRelay(void *pvParameters);
 void taskTcpServer(void *pvParameters); // direct connection
@@ -147,10 +121,6 @@ void setup()
             delay(2000);
         }
     }
-    /// telnet
-    // telnetServer.begin();
-    // telnetServer.setNoDelay(true);
-    ////
 
     debug_tools::logMessage("Initialize watchdog");
     esp_task_wdt_init(WDT_TIMEOUT, true);
@@ -170,14 +140,9 @@ void setup()
                  { debug_tools::logMessage("Error[%u]: ", error); });
 
     ArduinoOTA.begin();
-
-    // xTaskCreatePinnedToCore(taskReceiveRelayCommand, "taskReceiveRelayCommand", 4096 * 4, NULL, 1, &hRelayTask, 0);
     communication::initQueue();
     // main mqtt task
     xTaskCreatePinnedToCore(taskMQTT, "taskMQTT", 2048 * 4, NULL, 1, &hMQTTTask, 0);
-#ifdef DEVICE_BME_280_ENABLED
-    xTaskCreate(taskReadBME280, "taskReadBME280", 2048 * 4, NULL, 1, &hBME280Task);
-#endif
 // hardware sensors tasks
 #ifdef DEVICE_RELAY_ENABLED
     xTaskCreate(taskRelay, "taskRelay", 4096, NULL, 1, &hRelayTask);
@@ -259,7 +224,6 @@ void taskRelay(void *pv)
                 digitalWrite(RELAY_PIN, HIGH);
             }
         }
-        // vTaskDelay(pdMS_TO_TICKS(100));
     }
 }
 
@@ -284,45 +248,6 @@ void mycallback(char *topic, byte *message, unsigned int length)
     communication::RelayCommand cmd;
     cmd.on = (msgTemp == "ON");
     xQueueSend(communication::relayQueue, &cmd, 0); // queue is thread safe
-}
-
-void taskReadBME280(void *pvParameters)
-{
-    esp_task_wdt_add(NULL);
-
-    char buf[16];
-    Wire.begin(D4, D5); // 6 7
-    communication::MqttMessage msg;
-
-    if (!bme.begin(0x76, &Wire))
-    {
-        if (!bme.begin(0x77, &Wire))
-        {
-            debug_tools::logMessage("Could not find a valid BME280 sensor, check wiring!");
-            vTaskDelete(NULL); // delete task immediately if fails
-        }
-    }
-    for (;;)
-    {
-        snprintf(buf, sizeof(buf), "%.2f", bme.readTemperature());
-        msg.setContent(BME_TEMPERATURE_TOPIC, buf);
-        msg.sendToQueue();
-
-        snprintf(buf, sizeof(buf), "%.2f", bme.readPressure());
-        msg.setContent(BME_PRESSURE_TOPIC, buf);
-        msg.sendToQueue();
-
-        snprintf(buf, sizeof(buf), "%.2f", bme.readAltitude(SEALEVELPRESSURE_HPA));
-        msg.setContent(BME_ALTITUDE_TOPIC, buf);
-        msg.sendToQueue();
-
-        snprintf(buf, sizeof(buf), "%.2f", bme.readHumidity());
-        msg.setContent(BME_HUMIDITY_TOPIC, buf);
-        msg.sendToQueue();
-
-        esp_task_wdt_reset();
-        vTaskDelay(pdMS_TO_TICKS(1000));
-    }
 }
 
 // direct tcp
