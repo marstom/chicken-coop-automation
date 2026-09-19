694a671d9d9f3ef3dda780249f9ec64ace62014c
Tomasz Marszalek <marstomek@gmail.com>
2026-09-19 08:41:58 +0200

    reformat

--- libraries/tiny_yellow_blue_display/tiny_yellow_blue_display.cpp
+++ libraries/tiny_yellow_blue_display/tiny_yellow_blue_display.cpp
@@ -1,15 +1,13 @@
 /// TODO display driver...
 
-
-
-#include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <Wire.h>
 #include <driver/gpio.h>
 
+#include "common/am2320/am2320.h"
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
-#include "common/am2320/am2320.h"
 
 #define SDA_PIN GPIO_NUM_1
 #define SCL_PIN GPIO_NUM_2
@@ -17,97 +15,93 @@
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
 
-Adafruit_SSD1306 display(
-    SCREEN_WIDTH,
-    SCREEN_HEIGHT,
-    &Wire,
-    -1);
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
 int positionX = 0;
 int positionY = 0;
 
-void taskDisplay(void *pvParameters);
+void taskDisplay(void* pvParameters);
 
 void setup()
 {
-    Serial.begin(115200);
+  Serial.begin(115200);
 
-    Wire.begin(SDA_PIN, SCL_PIN);
+  Wire.begin(SDA_PIN, SCL_PIN);
 
-    if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
-    {
-        Serial.println("OLED not found");
-        while (true)
-            ;
-    }
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
+  {
+    Serial.println("OLED not found");
+    while (true)
+      ;
+  }
 
-    display.clearDisplay();
+  display.clearDisplay();
 
-    display.setTextColor(SSD1306_WHITE);
-    display.setTextSize(2);
-    // display.println("Hello!");
-    // display.setCursor(1, 15);
-    // display.println("WORLD!");
-    // display.setCursor(1, 45);
-    // display.println("Hej!");
-    display.display();
+  display.setTextColor(SSD1306_WHITE);
+  display.setTextSize(2);
+  // display.println("Hello!");
+  // display.setCursor(1, 15);
+  // display.println("WORLD!");
+  // display.setCursor(1, 45);
+  // display.println("Hej!");
+  display.display();
 
-    // temperature sensor
+  // temperature sensor
 
-    my_am2320::init(SDA_PIN, SCL_PIN);
+  my_am2320::init(SDA_PIN, SCL_PIN);
 
-    xTaskCreate(taskDisplay, "taskDisplay", 4096, NULL, 1, NULL);
+  xTaskCreate(taskDisplay, "taskDisplay", 4096, NULL, 1, NULL);
 }
 
 void loop()
 {
-    // display.clearDisplay();
-    // positionX += 1;
-    // positionY += 1;
-    // display.setCursor(positionX, positionY);
-    // display.println("Tomek");
-    // display.display();
-    // // delay(80);
+  // display.clearDisplay();
+  // positionX += 1;
+  // positionY += 1;
+  // display.setCursor(positionX, positionY);
+  // display.println("Tomek");
+  // display.display();
+  // // delay(80);
 
-    // if (positionX > SCREEN_WIDTH) {
-    //     positionX = 0;
-    // }
-    // if (positionY > SCREEN_HEIGHT) {
-    //     positionY = 0;
-    // }
+  // if (positionX > SCREEN_WIDTH) {
+  //     positionX = 0;
+  // }
+  // if (positionY > SCREEN_HEIGHT) {
+  //     positionY = 0;
+  // }
 }
 
-void taskDisplay(void *pvParameters)
+void taskDisplay(void* pvParameters)
 {
-    float temperature = 0.0;
-    float humidity = 0.0;
-    while (true)
-    {
-        //     display.clearDisplay();
-        //     positionX += 1;
-        //     positionY += 1;
-        //     display.setCursor(positionX, positionY);
-        //     display.println("Tomek");
-        //     display.display();
-        //     delay(50);
+  float temperature = 0.0;
+  float humidity = 0.0;
+  while (true)
+  {
+    //     display.clearDisplay();
+    //     positionX += 1;
+    //     positionY += 1;
+    //     display.setCursor(positionX, positionY);
+    //     display.println("Tomek");
+    //     display.display();
+    //     delay(50);
 
-        //     if (positionX > SCREEN_WIDTH) {
-        //         positionX = 0;
-        //     }
-        //     if (positionY > SCREEN_HEIGHT) {
-        //         positionY = 0;
-        Serial.println(my_am2320::measure_temperature());
-        //     }
+    //     if (positionX > SCREEN_WIDTH) {
+    //         positionX = 0;
+    //     }
+    //     if (positionY > SCREEN_HEIGHT) {
+    //         positionY = 0;
+    Serial.println(my_am2320::measure_temperature());
+    //     }
 
-        display.setCursor(positionX, positionY);
-        temperature = my_am2320::measure_temperature();
-        humidity = my_am2320::measure_humidity();
-        display.println("Temp: ");
-        display.println(temperature);
-        display.println("Hum: ");
-        display.println(humidity);
-        display.display();
-        vTaskDelay(500 / portTICK_PERIOD_MS);
-        display.clearDisplay();
-    }
+    display.setCursor(positionX, positionY);
+    temperature = my_am2320::measure_temperature();
+    humidity = my_am2320::measure_humidity();
+    display.println("Temp: ");
+    display.println(temperature);
+    display.println("Hum: ");
+    display.println(humidity);
+    display.display();
+    vTaskDelay(500 / portTICK_PERIOD_MS);
+    display.clearDisplay();
+  }
 }
--- libraries/tiny_yellow_blue_display/tiny_yellow_blue_display.h
+++ libraries/tiny_yellow_blue_display/tiny_yellow_blue_display.h
@@ -1,12 +1,10 @@
 #include <Arduino.h>
 
-
-
 namespace tiny_yellow_blue_display
 {
-    void init();
-    void displayText(const char *text, int x, int y);
-    void displayBitmap(const uint8_t *bitmap, int x, int y);
-    void displayVerticalBar(const int height, int x, int y);
-    void clearDisplay();
-}
+  void init();
+  void displayText(const char* text, int x, int y);
+  void displayBitmap(const uint8_t* bitmap, int x, int y);
+  void displayVerticalBar(const int height, int x, int y);
+  void clearDisplay();
+} // namespace tiny_yellow_blue_display
